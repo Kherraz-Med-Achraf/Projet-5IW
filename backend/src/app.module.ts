f9744d65a61304f3c1d0ca9abdcaa476a3ac6338
Kherraz-Med-Achraf <achraf.sena@gmail.com>
2025-04-21 15:22:51 +0000

    feat: update Dockerfile and configuration for production environment with secrets management

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as fs from 'fs';
+
+
 
 @Module({
   imports: [
@@ -16,10 +19,12 @@ import { AppService } from './app.service';
       host: process.env.POSTGRES_HOST || 'postgres',
       port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
       username: process.env.POSTGRES_USER || 'myuser',
-      password: process.env.POSTGRES_PASSWORD || 'mypassword',
+      password: process.env.POSTGRES_PASSWORD || fs.readFileSync('/run/secrets/pg_password', 'utf8').trim(),
       database: process.env.POSTGRES_DB || 'mydb',
       autoLoadEntities: true,
       synchronize: true, // à utiliser uniquement en dev
+      retryAttempts: 20,    // nombre maximal de tentatives
+      retryDelay: 3000,     // délai (ms) entre chaque tentative
     }),
   ],
   controllers: [AppController],

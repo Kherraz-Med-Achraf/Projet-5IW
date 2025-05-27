0bad47072a0058cdbffd865e909e364509f6a385
Kherraz-Med-Achraf <achraf.sena@gmail.com>
2025-05-27 14:17:46 +0000

    feat: add health module and controller; enhance Docker Compose with resource reservations and health checks

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,37 +1,42 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER } from "@nestjs/core";
-import { SentryModule, SentryGlobalFilter } from "@sentry/nestjs/setup";
+import { APP_FILTER } from '@nestjs/core';
+import { SentryModule, SentryGlobalFilter } from '@sentry/nestjs/setup';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as fs from 'fs';
-
-
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
-    SentryModule.forRoot(), 
+    SentryModule.forRoot(),
     ConfigModule.forRoot({
-      isGlobal: true, 
+      isGlobal: true,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST || 'postgres',
       port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
       username: process.env.POSTGRES_USER || 'myuser',
-      password: process.env.POSTGRES_PASSWORD || fs.readFileSync('/run/secrets/pg_password', 'utf8').trim(),
+      password:
+        process.env.POSTGRES_PASSWORD ||
+        fs.readFileSync('/run/secrets/pg_password', 'utf8').trim(),
       database: process.env.POSTGRES_DB || 'mydb',
       autoLoadEntities: true,
       synchronize: true, // à utiliser uniquement en dev
       retryAttempts: 30,
       retryDelay: 5000,
     }),
+    HealthModule,
   ],
   controllers: [AppController],
-  providers: [AppService, {
-    provide: APP_FILTER,
-    useClass: SentryGlobalFilter,
-  }],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: SentryGlobalFilter,
+    },
+  ],
 })
 export class AppModule {}
